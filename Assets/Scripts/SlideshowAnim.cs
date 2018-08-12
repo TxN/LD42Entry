7b6a07cd9fb2e45d1ac251ed478fbe388cce8ad9
Anton <AimII@yandex.ru>
2018-08-12 17:19:08 +0600

    Meta and slideshow anim

---
+++ Assets/Scripts/SlideshowAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideshowAnim : MonoBehaviour {
+    public List<Sprite> Sprites = new List<Sprite>();
+    public List<float>  Delays = new List<float>();
+
+    float _time = 0f;
+    float _nextFrameTime = 0f;
+    int _frameIndex = 0;
+    SpriteRenderer _renderer = null;
+
+    void Start() {
+        _renderer = GetComponent<SpriteRenderer>();
+        _nextFrameTime =Time.time + Delays[_frameIndex];
+        _renderer.sprite = Sprites[0];
+        _frameIndex++;
+    }
+
+    void Update() {
+        _time += Time.deltaTime;
+        if (Time.time > _nextFrameTime) {
+            _renderer.sprite = Sprites[_frameIndex];
+            _nextFrameTime = Time.time + Delays[_frameIndex];
+            _frameIndex++;
+            if (_frameIndex >= Sprites.Count) {
+                _frameIndex = 0;
+            }
+        }
+    }
+}
